701e1edc5c6e0c5ba7eb8a356317b3b9c19b6f85
Mohamed Zahi <mohamedzahi33@gmail.com>
2025-10-28 12:29:55 +0400

    replace json with html form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, Form
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, EmailStr
 from functions.append_row_sheet import append_row
@@ -14,20 +14,31 @@ SHEET_NAME = "Sheet1"
 app = FastAPI()
 
 
-class EmailWebhook(BaseModel):
-    email: EmailStr
-
-
 @app.post("/bigtree-newsletter-email-webhook-v2-1-webhook")
-async def webhook_1(payload: EmailWebhook):
-    email = payload.email
+async def webhook_1(email: str = Form(...)):
     row = ["No Name", email, "Subscribed"]
     
     success = append_row(SHEET_ID, SHEET_NAME, row)
     if not success:
         return JSONResponse(status_code=500, content={"status": "fail", "detail": "Failed to append row to Google Sheet"})
 
-    return Response(status_code=status.HTTP_200_OK)
+    return {"status": "success"}
+
+
+
+# class EmailWebhook(BaseModel):
+#     email: EmailStr
+
+# @app.post("/bigtree-newsletter-email-webhook-v2-1-webhook")
+# async def webhook_1(payload: EmailWebhook):
+#     email = payload.email
+#     row = ["No Name", email, "Subscribed"]
+    
+#     success = append_row(SHEET_ID, SHEET_NAME, row)
+#     if not success:
+#         return JSONResponse(status_code=500, content={"status": "fail", "detail": "Failed to append row to Google Sheet"})
+
+#     return Response(status_code=status.HTTP_200_OK)
 
 
 
