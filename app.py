91020d6d84f9587f28903a9165c9803051832ee2
Mohamed Zahi <mohamedzahi33@gmail.com>
2025-11-25 12:28:26 +0400

    .

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Response, status, Request, BackgroundTasks
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel, EmailStr, ValidationError
