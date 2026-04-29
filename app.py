165e0780e9a21c7fd2b8d2ef248c127b0586aec3
Mohamed Zahi <mohamedzahi33@gmail.com>
2026-04-29 17:27:30 +0400

    .

--- app.py
+++ app.py
@@ -54,11 +54,11 @@ class ContactRequest(BaseModel):
     src: str | None = None
 
 def process_contact_request(fname, lname, email, phone, company, role, project, project_location, message, src):
-    print("Processing contact request for", email)
     try:
         row = [fname, lname, email, phone, company, project, project_location, message, src, datetime.now(timezone(timedelta(hours=4))).strftime("%Y-%m-%d %H:%M:%S"), role]
         append_row(SHEET_ID, "Contact", row)
-        sf_result = sf.insert_contact_form(first_name=fname, last_name=lname, email=email, mobile=phone, company=company, role=role, project=project, project_location=project_location, message=message, src=src)
+        
+        sf_result = sf.insert_contact_form(first_name=fname, last_name=lname, email=email, mobile=phone, company=company, role=role, country_code=project_location, project=project, general_notes=message)
         print('sf_result: ', sf_result)
 
     except Exception as e:
